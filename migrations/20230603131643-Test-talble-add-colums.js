0369bdb0290d38b51e6d91267429269ee8268ba0
ANDRII_LIASOTA <lyasota.andriy@gmail.com>
2023-06-03 16:43:40 +0300

    migrations

---
+++ migrations/20230603131643-Test-talble-add-colums.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Test', 'age', {
+      type: Sequelize.DataTypes.SMALLINT,
+      allowNull: true
+    })
+
+    await queryInterface.addColumn('Test', 'description', {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: true
+    })
+
+    await queryInterface.addColumn('Test', 'comments', {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: true
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    queryInterface.removeColumn('age')
+    queryInterface.removeColumn('description')
+    queryInterface.removeColumn('comments')
+  }
+};
